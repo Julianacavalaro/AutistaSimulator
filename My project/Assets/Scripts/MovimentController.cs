3fdf0eb4af4a22ce004270bfd725de6973f43f32
Julianacavalaro <juliana.cavalaro@hotmail.com>
2025-12-06 17:49:36 -0300

    Ajusta olhar para baixo quando parado

--- My project/Assets/Scripts/MovimentController.cs
+++ My project/Assets/Scripts/MovimentController.cs
@@ -7,95 +7,62 @@ public class MovimentController : MonoBehaviour
     public float speed = 5f;
     private Rigidbody2D rd;
     private Vector2 moviment;
-    private Vector2 lastMoviment = new Vector2(0, -1);
     private Animator animator;
     private bool isMoving = false;
 
     void Awake()
-        {
-        Debug.Log("AWAKE do objeto: " + gameObject.name +
-                      " | InstanceID: " + GetInstanceID());
-        rd = GetComponent<Rigidbody2D>(); //peguei o componente
+    {
+        rd = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
-    /* 
-       private void OnEnable() 
-       {
-           Debug.Log("ON ENABLE do objeto: " + gameObject.name +
-                     " | InstanceID: " + GetInstanceID());
-       }
-       private void OnDisable()
-       {
-           Debug.Log("ON DISABLE do objeto: " + gameObject.name +
-                     " | InstanceID: " + GetInstanceID());
-       }*/
-    // Start is called before the first frame update
-
     void Start()
     {
-        Debug.Log("START do objeto: " + gameObject.name +
-                  " | InstanceID: " + GetInstanceID());
- 
-            // garante que o Animator já comece na direção certa
-            animator.SetFloat("Horizontal", 0f);
-            animator.SetFloat("Vertical", -1f);  // baixo
-            animator.SetBool("isMoving", false);
-        
+        // garante que o Animator já comece na direção certa
+        animator.SetFloat("Horizontal", 0f);
+        animator.SetFloat("Vertical", -1f);  // baixo
+        animator.SetBool("isMoving", false);
     }
-
-    // Update is called once per frame
     void Update()
     {
-        Debug.Log("UPDATE do objeto: " + gameObject.name +
-                  " | InstanceID: " + GetInstanceID());
         moviment.x = Input.GetAxis("Horizontal");
         moviment.y = Input.GetAxis("Vertical");
 
-        Debug.Log("Horizontal: " + moviment.x + " | Vertical: " + moviment.y);
-
         if (moviment.x != 0 || moviment.y != 0)
         {
-
             isMoving = true;
-            lastMoviment = moviment;
-
         }
         else
         {
-          
             isMoving = false;
         }
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-
             speed *= 2;
-  
-        }
+
         if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-
             speed /= 2;
+    }
+
+    void FixedUpdate()
+    {
+        rd.velocity = moviment * speed;
+    }
+
+    void LateUpdate()
+    {
+        if (isMoving)
+        {
+            // andando → usa a direção real
+            animator.SetFloat("Horizontal", moviment.x);
+            animator.SetFloat("Vertical", moviment.y);
+        }
+        else
+        {
+            // parado → sempre olhando para baixo (MoveDown)
+            animator.SetFloat("Horizontal", 0f);
+            animator.SetFloat("Vertical", -1f);
         }
 
-    }
-    private void FixedUpdate()
-    {
-        Debug.Log("FIXED UPDATE do objeto: " + gameObject.name +
-                  " | InstanceID: " + GetInstanceID());
-        rd.velocity = moviment * speed;
-
-    }
-
-    private void LateUpdate()
-    {
-        Debug.Log("LATE UPDATE do objeto: " + gameObject.name +
-                  " | InstanceID: " + GetInstanceID());
-
-        Vector2 animatorMoviment = isMoving ? moviment : lastMoviment;
-        animator.SetFloat("Horizontal", animatorMoviment.x);
-        animator.SetFloat("Vertical", animatorMoviment.y);
         animator.SetBool("isMoving", isMoving);
-      //  animator.SetFloat("Speed", moviment.sqrMagnitude);
     }
 }
